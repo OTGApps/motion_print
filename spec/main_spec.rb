ffe770470435287bd41b7415c41173807f9a9a2a
Mark Rickert <mjar81@gmail.com>
2014-07-11 13:38:39 -0500

    Finished specs.

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -18,39 +18,59 @@ describe "motion_print gem" do
   end
 
   it 'aligns strings properly' do
-    1.should == 1
+    MotionPrint.align('RubyMotion', 0, 0).should == 'RubyMotion'
+    MotionPrint.align('RubyMotion', 5, 0).should == 'RubyMotion'
+    MotionPrint.align('RubyMotion', 20, 0).should == 'RubyMotion          '
+    MotionPrint.align('RubyMotion', 20, 1).should == '  RubyMotion          '
+    MotionPrint.align('RubyMotion', 20, 2).should == '    RubyMotion          '
+    MotionPrint.align('RubyMotion', 20, 5).should == '          RubyMotion          '
+    MotionPrint.align('RubyMotion', 5, 2).should == '    RubyMotion'
+    MotionPrint.align('RubyMotion', 0, 1).should == '  RubyMotion'
   end
 
   it 'outputs arrays properly' do
-    1.should == 1
+    # Numbers
+    MotionPrint.logger([1, 2]).should == "[\n  \e[1;34m1\e[0m,\n  \e[1;34m2\e[0m\n]"
+    # Strings
+    MotionPrint.logger(['1', '2']).should == "[\n  \e[0;33m1\e[0m,\n  \e[0;33m2\e[0m\n]"
   end
 
   it 'outputs multidimensional arrays properly' do
-    1.should == 1
+    # Numbers and Strings
+    MotionPrint.logger([1, 2, ['1', '2']]).should == "[\n  \e[1;34m1\e[0m,\n  \e[1;34m2\e[0m,\n  [\n    \e[0;33m1\e[0m,\n    \e[0;33m2\e[0m\n  ]\n]"
   end
 
   it 'outputs hashes properly' do
-    1.should == 1
+    MotionPrint.logger({a: 'a', b: 2}).should == "{\n  \e[0;36m:a\e[0m  \e[0;36m => \e[0m\e[0;33ma\e[0m,\n  \e[0;36m:b\e[0m  \e[0;36m => \e[0m\e[1;34m2\e[0m\n}"
   end
 
   it 'outputs hashes with arrays properly' do
-    1.should == 1
+    MotionPrint.logger({
+      a: 'a',
+      b: 2,
+      c: [
+        BigDecimal.new('4.4'),
+        12,
+        String
+      ]
+    }).should == "{\n  \e[0;36m:a\e[0m  \e[0;36m => \e[0m\e[0;33ma\e[0m,\n  \e[0;36m:b\e[0m  \e[0;36m => \e[0m\e[1;34m2\e[0m,\n  \e[0;36m:c\e[0m  \e[0;36m => \e[0m[\n    \e[1;34m4.4\e[0m,\n    \e[1;34m12\e[0m,\n    \e[1;33mString\e[0m\n  ]\n}"
   end
 
   it 'outputs numbers correctly' do
-    1.should == 1
+    MotionPrint.logger(42).should == "\e[1;34m42\e[0m"
   end
 
   it 'outputs classes correctly' do
-    1.should == 1
+    MotionPrint.logger(Array).should == "\e[1;33mArray\e[0m"
   end
 
   it 'outputs symbols correctly' do
-    1.should == 1
+    MotionPrint.logger(:symbol).should == "\e[0;36m:symbol\e[0m"
   end
 
   it 'outputs true/false correctly' do
-    1.should == 1
+    MotionPrint.logger(true).should == "\e[1;32mtrue\e[0m"
+    MotionPrint.logger(false).should == "\e[1;31mfalse\e[0m"
   end
 
   # ...
